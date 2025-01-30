32c26e906cafb0e553ee275d25de07beac4c1227
deisibonilla <deisi@growingminds.nl>
2025-01-30 15:34:55 +0100

    Added list group

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,27 @@
+
+function ListGroup() {
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ]
+    return (
+        <>
+            <h1>List</h1>
+            <ul className="list-group">
+                { items.map(item =>
+                    <li
+                        className="list-group-item"
+                        key={item}
+                    >
+                        {item}
+                    </li>
+                )}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
